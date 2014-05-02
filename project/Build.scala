e001e2c4e066811974971d3a3a8af737cdadbda0
Marco Firrincieli <marco.firrincieli@unicreditgroup.eu>
2014-05-02 12:08:04 +0200

    Added more license information and Build.scala extra info

--- project/Build.scala
+++ project/Build.scala
@@ -1,3 +1,18 @@
+/*  Copyright 2014 UniCredit S.p.A.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import sbt._
 import Keys._
 import spray.revolver.RevolverPlugin._
@@ -11,8 +26,12 @@ object BuildSettings {
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "unicredit",
+    organizationName := "UniCredit S.p.A.",
+    organizationHomepage := Some(new URL("https://www.unicreditgroup.eu")),
+    licenses := Seq("Apache License v2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
     version := "1.0",
     scalaVersion := "2.10.3",
+    scmInfo := Some(ScmInfo(url("https://github.com/unicredit/hippodb"),"git://github.com/unicredit/hippodb.git")),    
     scalacOptions ++= Seq(
       "-deprecation",
       "-feature",
