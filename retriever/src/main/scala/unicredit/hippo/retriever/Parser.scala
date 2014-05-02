c16d16fd9743b6134745a952734389d26a0bc9c2
Andrea Ferretti <ferrettiandrea@gmail.com>
2014-05-02 18:33:42 +0200

    Fixed cli argument parsing

--- retriever/src/main/scala/unicredit/hippo/retriever/Parser.scala
+++ retriever/src/main/scala/unicredit/hippo/retriever/Parser.scala
@@ -28,16 +28,16 @@ object Parser {
 
   val parser = new OptionParser[Config]("hippo-db retriever") {
     head("benchmark parser", "0.1")
-    opt[String]('s', "source") valueName("<path>") action { (x, c) =>
+    opt[String]('s', "source") required() valueName("<path>") action { (x, c) =>
       c.copy(source = x)
     } text("the directory to retrieve from HDFS")
-    opt[String]('p', "table") valueName("<name>") action { (x, c) =>
-      c.copy(source = x)
+    opt[String]('p', "table") required() valueName("<name>") action { (x, c) =>
+      c.copy(table = x)
     } text("the table to retrieve")
-    opt[String]('i', "id") valueName("<name>") action { (x, c) =>
+    opt[String]('i', "id") required() valueName("<name>") action { (x, c) =>
       c.copy(id = x)
     } text("the local id of this server")
-    opt[String]('t', "target") valueName("<path>") action { (x, c) =>
+    opt[String]('t', "target") required() valueName("<path>") action { (x, c) =>
       c.copy(target = x)
     } text("the local destination")
     help("help") text("prints this usage text")
